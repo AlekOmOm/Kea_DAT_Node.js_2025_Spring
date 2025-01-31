2851b7f36fb5c5ed788c108bb83b5b480a2adf14
AlekOmOm <AlekOmOm@users.noreply.github.com>
2025-01-31 12:15:55 +0100

    1 basics done

--- 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
+++ 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
@@ -9,6 +9,15 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log(`My first name is ${firstName} and my last name is ${lastName}`)
+
+const intro = (first, last) => {
+   console.log(`My first name is ${first} and my last name is ${last}`)
+}
+
+intro(firstName,lastName)
+
+
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -21,4 +30,19 @@ const increment = 1;
 // You cannot touch the first or the second line
 
 
+// in js, Integer data type doesnt exist
+// float is a Number 
+// 
+const new_year = parseInt(year) + increment
+const new_year2 = Number(year)+1
+const new_year3 = +year + increment // +year converts it to a number
+
+console.log(new_year)
+console.log(new_year2)
+console.log(new_year3)
+
+const isItANumber = "3424dsf"
+
+console.log(Number(isItANumber))
+console.log("error code", parseInt(isItANumber))
 // --------------------------------------
