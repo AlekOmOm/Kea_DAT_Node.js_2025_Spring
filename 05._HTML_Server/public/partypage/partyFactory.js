119d5843c3b79f36fb48167d2c48971a8a6487c6
AlekOmOm <AlekOmOm@users.noreply.github.com>
2025-02-28 10:47:29 +0100

    modified app.28-02.js public/partypage module

---
+++ 05._HTML_Server/public/partypage/partyFactory.js
@@ -0,0 +1,7 @@
+
+function partyFactory() {
+  return {
+    partyName: 'Birthday Party',
+    props: 'Balloons, Cake, Candles',
+  };
+}
---
+++ 05._HTML_Server/util/partiesLibraryES.js
@@ -0,0 +1,20 @@
+// with ES Module 
+    /*
+     *
+     *
+     *
+     *
+     */
+
+
+
+console.log("parties library ES module");
+
+// ES = EcmaScript
+
+
+export default function partiesLibraryES() {
+  console.log("parties library ES module function");
+
+}
+
